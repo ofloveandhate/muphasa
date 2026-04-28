653b37c7d747a0b10697c5d7e40e84855de939f8
Katherine Benjamin <kate@katherinebenjamin.co.uk>
2026-04-28 15:27:03 +0200

    remove unused import

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 import os
 import platform
-from setuptools import setup, Extension, find_packages
-from setuptools.command.build_ext import build_ext
-from Cython.Build import cythonize
+
 import numpy
-
-
+from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
 
 # Compiler flags
 extra_compile_args = ["-O3", "-ffast-math", "-D_hypot=hypot"]
@@ -30,24 +28,27 @@ ext_modules = [
             "src/cpp/IO.cpp",
             "src/cpp/matrix.cpp",
             "src/cpp/utils.cpp",
-            "src/cpp/presentation.cpp"
+            "src/cpp/presentation.cpp",
         ],
         include_dirs=[
-            "src/python/mph",   # for .pxd files
-            "src/cpp",      # for C++ headers
+            "src/python/mph",  # for .pxd files
+            "src/cpp",  # for C++ headers
             numpy.get_include(),
-            os.environ.get("BOOST_INC_DIR", "/opt/homebrew/include" if system == "Darwin" else "/usr/local/include")
+            os.environ.get(
+                "BOOST_INC_DIR",
+                "/opt/homebrew/include" if system == "Darwin" else "/usr/local/include",
+            ),
         ],
         language="c++",
         extra_compile_args=extra_compile_args,
-        extra_link_args=extra_link_args
+        extra_link_args=extra_link_args,
     )
 ]
 
 setup(
     ext_modules=cythonize(ext_modules, language_level=3),
-    packages = find_packages(where="src/python"),
-    package_dir = {"": "src/python"},
+    packages=find_packages(where="src/python"),
+    package_dir={"": "src/python"},
     # package_dir={"":"src/python"},
-    zip_safe=False
+    zip_safe=False,
 )
